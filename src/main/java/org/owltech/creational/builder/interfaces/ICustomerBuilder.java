e9c446153a6c1075471f73af081b6f757356f645
Praful Makani <pmakani@Prafuls-MacBook-Pro.local>
2023-11-02 22:39:14 +0530

    format code

--- src/main/java/org/owltech/creational/builder/interfaces/ICustomerBuilder.java
+++ src/main/java/org/owltech/creational/builder/interfaces/ICustomerBuilder.java
@@ -1,9 +1,11 @@
 package org.owltech.creational.builder.interfaces;
 
 public interface ICustomerBuilder {
-
     ICustomerBuilder firstName(String firstName);
+
     ICustomerBuilder lastName(String lastName);
+
     ICustomerBuilder primaryEmail(String email);
+
     ICustomerBuilder mobile(String mobile);
 }
